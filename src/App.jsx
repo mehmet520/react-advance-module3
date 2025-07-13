3d3aedd6fb1b2dd209c88a0535688856aaf949b9
Mehmet Yilmaz <85320832+mehmet520@users.noreply.github.com>
2025-07-13 19:54:32 +0200

    chore: add husky pre-commit hook and lint-staged configuration for code quality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg';
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const y = 1
+  console.log('y :>> ', y)
   return (
     <>
       <div>
